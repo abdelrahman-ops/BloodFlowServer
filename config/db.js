8abf0d7ca48bfb08f6a5300b53cef77cb4b65bdb
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-24 01:41:53 +0200

    set db connection function

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        mongoose.set("strictPopulate", false);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
