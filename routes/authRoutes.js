970f90dd0abc82915f730505f814ef0ee7d89d66
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-24 05:59:20 +0200

    add admin register route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { loginUser, registerUser, logoutUser } from "../controllers/authController.js";
+import { loginUser, registerUser, logoutUser, registerAdmin } from "../controllers/authController.js";
 
 const router = express.Router();
 
 // User Registration
 router.post("/register", registerUser);
+
+// Admin Registration
+router.post("/register/admin", registerAdmin);
 
 // User Login
 router.post("/login", loginUser);
