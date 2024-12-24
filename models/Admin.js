0b42d15f613d4a8c566ceca3dadf13b633b96159
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2024-12-24 05:58:41 +0200

    modify schema

--- models/Admin.js
+++ models/Admin.js
@@ -1,5 +1,5 @@
-const { required } = require("joi");
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const adminSchema = new mongoose.Schema(
   {
@@ -9,7 +9,7 @@ const adminSchema = new mongoose.Schema(
     hospital: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Hospital",
-      required: True,
+      required: true,
     },
     role: {
       type: String,
@@ -25,6 +25,20 @@ const adminSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+adminSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 
 const Admin = mongoose.model("Admin", adminSchema);
 export default Admin;
