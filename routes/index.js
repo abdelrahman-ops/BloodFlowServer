086f488dfb9ba904162f9c1a14e9b1c1299abab3
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2025-08-05 16:10:17 +0300

    back to version 1.1

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,6 @@ import notificationRoutes from "./notificationRoutes.js";
 import hospitalRoutes from "./hospitalRoutes.js";
 import patientRoutes from './patientRoutes.js';
 
-import admin from './admin.routes.js';
-import auth from './auth.routes.js';
-import requests from './request.routes.js';
-import emergency from './emergency.routes.js'
-import donors from './donor.routes.js';
-import notification from './notification.routes.js'
 
 const router = express.Router();
 
@@ -21,12 +15,6 @@ router.use("/notifications", notificationRoutes);
 router.use("/hospitals", hospitalRoutes);
 router.use("/patient", patientRoutes);
 
-router.use('/auth/v2', auth);
-router.use('/requests/v2', requests);
-router.use('/emergency/v2', emergency);
-router.use('/donors/v2', donors);
-router.use('/admin/v2', admin);
-router.use('/notification/v2', notification);
 
 export default router;
 
