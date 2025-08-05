662e58bfef8a1b30d6f51ecf4f39f8df40530e12
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2025-08-05 16:51:47 +0300

    test version 2 - fix 4 - test with 'admin route + not + donors'

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ import admin from './admin.routes.js';
 // import auth from './auth.routes.js';
 // import requests from './request.routes.js';
 // import emergency from './emergency.routes.js'
-// import donors from './donor.routes.js';
+import donors from './donor.routes.js';
 import notification from './notification.routes.js'
 
 const router = express.Router();
@@ -25,7 +25,7 @@ router.use("/patient", patientRoutes);
 // router.use('/auth/v2', auth);
 // router.use('/requests/v2', requests);
 // router.use('/emergency/v2', emergency);
-// router.use('/donors/v2', donors);
+router.use('/donors/v2', donors);
 router.use('/admin/v2', admin);
 router.use('/notification/v2', notification);
 
