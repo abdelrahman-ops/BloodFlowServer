cdca939a58827712f60db9f16dbbfadc952d2efc
abdelrahman-obs <abdelrahmanataa17@gmail.com>
2025-08-05 20:11:51 +0300

    handle cors

--- index.js
+++ index.js
@@ -28,12 +28,15 @@ app.use(
   cors({
       origin: [
           "http://localhost:5173",
-          "https://blood-flow.vercel.app"
+          "https://blood-flow.vercel.app",
+          "https://blood-flow-server-v2.vercel.app"
       ],
       methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
       credentials: true,
   })
 );
+app.options("*", cors());
 app.use(express.json()); // Parse JSON request body
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded data
 app.use(morgan("dev")); // Logging
